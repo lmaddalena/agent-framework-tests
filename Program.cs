16f8281c07f4153ad9ced93064825331f08d4e5c
lmaddalena <mdn.luca@gmail.com>
2026-09-02 12:33:44 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace agent_framework_tests;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("=== MAIN MENU ===");
+            Console.WriteLine("1. First Agent");
+            Console.WriteLine("2. TO DO...");
+            Console.WriteLine("3. TO DO...");
+            Console.WriteLine("4. Exit");
+            Console.Write("\nSelect an option (1-4): ");
+
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await FirstAgent.RunAsync();
+                    break;
+                case "2":
+                    Console.WriteLine("\nto do...");
+                    break;
+                case "3":
+                    Console.WriteLine("\nto do...");
+                    break;
+                case "4":
+                    exit = true;
+                    Console.WriteLine("\nExiting...");
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid choice. Press any key to continue.");
+                    break;
+            }
+
+            if (!exit)
+            {
+                Console.WriteLine("\nPress any key to continue...");
+                Console.Read();
+            }
+
+        }
+    }
+}
---
+++ first-agent.cs
@@ -0,0 +1,49 @@
+using System.Net.WebSockets;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+
+namespace agent_framework_tests;
+
+public static class FirstAgent
+{
+
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("\nFirst Agent:");
+        Console.WriteLine("-------------------------");
+
+        string modelId = "llama3.2:latest";
+        string endpoint = "http://localhost:11434";
+
+        // Setup logger factory for Microsoft.Extensions.AI
+        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Trace);
+        });
+
+        // create the logger
+        var logger = loggerFactory.CreateLogger("FirstAgent");
+
+        //logger.LogInformation("test logger");
+        
+        // Initialize the chat client using Microsoft.Extensions.AI abstraction
+        IChatClient ollamaClient = new OllamaChatClient(new Uri(endpoint), modelId)
+            .AsBuilder()
+            .UseLogging(loggerFactory)
+            .Build();
+
+        // Create an AI Agent
+        AIAgent agent = ollamaClient.AsAIAgent(
+            instructions: "You are a friendly assistant. Keep your answers brief.",
+            name: "HelloAgent"
+        );
+
+        string prompt = "What is the largest city in France?";
+        
+        Console.WriteLine($"User > {prompt}");
+
+        Console.WriteLine($"agent > {await agent.RunAsync(prompt)}");
+    }
+}
